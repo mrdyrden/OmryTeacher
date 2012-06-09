d24806e94be466d6d744665c17ffe908252bef78
Ron <ronchipi@gmail.com>
2012-06-09 12:43:49 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omryteacher::Application.config.secret_token = '59012385333845af01f19a81ce0965008db5f97ac95c7f4e4e889e7ede281012a6da06ef2a5ac2f56526650ffdb7a0e5bf18b8762c800e7c56a6e7e3aa2e4801'
