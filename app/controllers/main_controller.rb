4b8f61ee11e3de079ac4e37de297a0763b05002e
Ron <ronchipi@gmail.com>
2012-06-17 23:53:15 +0300

    added model validation

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,13 @@
 class MainController < ApplicationController
   def index
+    @student = Student.new
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @students }
+    end
+  end
+
+  def show
   end
 end
